8dc6cc0ea8a2d91e9feb47ad953bcd5daf1859de
Andrey <kezin636@gmail.com>
2023-11-07 23:41:07 +0300

    Nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,8 @@
 <script>
+	import Header from "$lib/components/Header/Header.svelte";
     import "$lib/styles/app.css";
 </script>
+
+<Header />
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,3 @@
 <svelte:head>
     <title>Main</title>
 </svelte:head>
-
-<h1 class="text-3xl font-bold">
-    Hello world!
-</h1>
